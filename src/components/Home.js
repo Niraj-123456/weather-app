8745d9c4bbce64fea358a248109d4866389b5820
Niraj-123456 <lamaniraj321@gmail.com>
2021-06-20 21:33:56 +0545

    navbar added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Home() {
     return (
-        <div>
-            <h2>This is Home page</h2>
-        </div>
+        <Container>
+            <NavBar>
+                <h4>Weather App</h4>
+            </NavBar>
+        </Container>
     )
 }
 
 export default Home
+
+const Container = styled.div`
+    width: 100%;
+    height: auto;
+`
+
+const NavBar = styled.div`
+    width: 100%;
+    height: 70px;
+    background-color: rgb(0, 0, 0);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+
+    h4 {
+        text-transform: uppercase;
+        font-size: 30px;
+        letter-spacing: 1px;
+    }
+`
